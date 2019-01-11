879b40389e918f321c927e6f9962a2154550aa20
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-01-11 11:21:18 +0200

    java insertion

--- script.js
+++ script.js
@@ -48,3 +48,30 @@ Referee.prototype.throw = function () {
         }
     }
 }
+Referee.prototype.hold = function () {
+    if (this.players[0].turn === 1) {
+        this.runningTotal(this.players[0]);
+        this.switchPlayer();
+
+    } else if (this.players[1].turn === 1) {
+        this.runningTotal(this.players[1]);
+        this.switchPlayer();
+
+    }
+}
+Referee.prototype.pickPlayer = function () {
+    this.players[0].turn = 1;
+}
+Referee.prototype.checkGame = function () {
+    if (this.players[0].score >= 100) {
+        this.players[1].turn = 0;
+        this.players[0].turn = 0;
+        this.winner = "Player 1 ";;
+        this.gameover = 1;
+    } else if (this.players[1].score >= 100) {
+        this.players[1].turn = 0;
+        this.players[0].turn = 0;
+        this.winner = "Player 2 ";
+        this.gameover = 1;
+    }
+}

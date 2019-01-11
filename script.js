c1eaf2f1e352a99327b733c68c16083f9fa377cd
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-01-11 12:07:21 +0200

    javascript insertions

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function Referee() {
     this.gameover = 0;
     this.dice = 0;
 }
+
 Referee.prototype.switchPlayer = function () {
     if (this.players[0].turn === 1) {
         this.players[0].turn = 0;
@@ -22,24 +23,25 @@ Referee.prototype.switchPlayer = function () {
     }
 
 }
+
 Referee.prototype.runningTotal = function (player) {
     player.score += player.runningTotal;
     player.runningTotal = 0;
 }
+
 Referee.prototype.throw = function () {
     var result = Math.floor((Math.random() * 6) + 1);
     if (this.gameover === 0) {
         this.dice = result;
     }
-    f(this.players[0].turn === 1) {
+    if (this.players[0].turn === 1) {
         if (result != 1) {
             this.players[0].runningTotal += result;
         } else {
             this.players[0].runningTotal = 0;
             this.switchPlayer();
         }
-    }
-    else if (this.players[1].turn === 1) {
+    } else if (this.players[1].turn === 1) {
         if (result != 1) {
             this.players[1].runningTotal += result;
         } else {
@@ -48,6 +50,7 @@ Referee.prototype.throw = function () {
         }
     }
 }
+
 Referee.prototype.hold = function () {
     if (this.players[0].turn === 1) {
         this.runningTotal(this.players[0]);
@@ -59,6 +62,7 @@ Referee.prototype.hold = function () {
 
     }
 }
+
 Referee.prototype.pickPlayer = function () {
     this.players[0].turn = 1;
 }
@@ -76,6 +80,17 @@ Referee.prototype.checkGame = function () {
     }
 }
 
+// Referee.prototype.checkAiTurn = function (){
+//   // console.log(this.players[1].playerID === "ai")
+//   if (this.players[1].playerID === "ai" && this.players[1].turn ===1){
+//     //throw dice once a second
+//     setInterval(this.throw, 1000);
+//     //if after 4 tosses hold
+//
+//   }
+// }
+
+//interface functions
 function switchClass(player1, player2) {
     if (player1.turn === 1) {
         $("div.player1").addClass("highlight");
@@ -103,82 +118,88 @@ function showDice(dice) {
 }
 
 $(document).ready(function () {
-    var gameChoice = $('input:radio[name=gameChoice]:checked').val();
-    var player1 = new Player("mister");
-    var player2 = new Player(gameChoice);
-    var jimmyTheReferee = new Referee();
+var gameChoice = $('input:radio[name=gameChoice]:checked').val();
+var player1 = new Player("mister");
+var player2 = new Player(gameChoice);
+var jimmyTheReferee = new Referee();
 
 
-    jimmyTheReferee.players.push(player1, player2);
-    jimmyTheReferee.pickPlayer();
-    $("#gameChoice").click(function () {
-        gameChoice = $('input:radio[name=gameChoice]:checked').val();
-        if (gameChoice === "ai") {
-            // console.log("ai");
-            jimmyTheReferee.players[1].playerID = "ai";
+jimmyTheReferee.players.push(player1, player2);
+jimmyTheReferee.pickPlayer();
+$("#gameChoice").click(function () {
+    gameChoice = $('input:radio[name=gameChoice]:checked').val();
+    if (gameChoice === "ai") {
+        // console.log("ai");
+        jimmyTheReferee.players[1].playerID = "ai";
 
-            //
+        //
 
-        }
-        $(this).parent().parent().fadeOut(700, function () {
-            $("#twoPlayer").fadeIn();
-        });
+    }
+    $(this).parent().parent().fadeOut(700, function () {
+        $("#twoPlayer").fadeIn();
     });
+});
 
-    setInterval(function () {
-        if (player2.playerID === "ai" && player2.turn === 1) {
-            if (player2.runningTotal <= 15) {
-                jimmyTheReferee.throw();
-                $("#hold").hide();
-                $("#roll").hide();
-            } else {
-                jimmyTheReferee.hold();
-                $("#hold").fadeIn();
-                $("#roll").fadeIn();
-            }
-            if (player2.turn === 0) {
-                $("#hold").fadeIn();
-                $("#roll").fadeIn();
-            }
-
-            jimmyTheReferee.checkGame();
-            if (jimmyTheReferee.gameover === 1) {
-                $("#winner").show().text(jimmyTheReferee.winner + "wins!!!");
-            }
-            showDice(jimmyTheReferee.dice);
-            showScore(player1, player2);
-            switchClass(player1, player2);
-
+setInterval(function () {
+    if (player2.playerID === "ai" && player2.turn === 1) {
+        if (player2.runningTotal <= 15) {
+            jimmyTheReferee.throw();
+            $("#hold").hide();
+            $("#roll").hide();
+        } else {
+            jimmyTheReferee.hold();
+            $("#hold").fadeIn();
+            $("#roll").fadeIn();
+        }
+        if (player2.turn === 0) {
+            $("#hold").fadeIn();
+            $("#roll").fadeIn();
         }
 
-    }, 1000);
-    // setInterval(jimmyTheReferee.checkAiTurn, 1000);
 
-    $("#roll").click(function () {
+
         jimmyTheReferee.checkGame();
-        jimmyTheReferee.throw();
-
-
+        if (jimmyTheReferee.gameover === 1) {
+            $("#winner").show().text(jimmyTheReferee.winner + "wins!!!");
+        }
         showDice(jimmyTheReferee.dice);
         showScore(player1, player2);
         switchClass(player1, player2);
 
-        if (jimmyTheReferee.gameover === 1) {
-            $("#winner").show().text(jimmyTheReferee.winner + "wins!!!");
-        }
-    });
+    }
 
-    $("#hold").click(function () {
+}, 1000);
+// setInterval(jimmyTheReferee.checkAiTurn, 1000);
 
-        jimmyTheReferee.hold();
-        showScore(player1, player2);
-        jimmyTheReferee.checkGame();
+$("#roll").click(function () {
+    jimmyTheReferee.checkGame();
+    jimmyTheReferee.throw();
 
-        switchClass(player1, player2);
-        showDice(player1, player2);
 
-        if (jimmyTheReferee.gameover === 1) {
-            $("#winner").show().text(jimmyTheReferee.winner + "wins!!!");
-        }
-    });
+    showDice(jimmyTheReferee.dice);
+    showScore(player1, player2);
+    switchClass(player1, player2);
+
+    if (jimmyTheReferee.gameover === 1) {
+        $("#winner").show().text(jimmyTheReferee.winner + "wins!!!");
+    }
+
+
+});
+
+$("#hold").click(function () {
+
+    jimmyTheReferee.hold();
+    showScore(player1, player2);
+    jimmyTheReferee.checkGame();
+
+    switchClass(player1, player2);
+    showDice(player1, player2);
+
+    if (jimmyTheReferee.gameover === 1) {
+        $("#winner").show().text(jimmyTheReferee.winner + "wins!!!");
+    }
+});
+});
+
 });
